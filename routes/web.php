2c033da33cd3661e0cae64a715aec29fb7de11c0
ErickHenrique <erick.2henriquehtp@gmail.com>
2025-05-03 22:21:39 -0300

    Feat: add

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,9 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
+    return ([
+      'message' =>  'GAROTO DE PROGRAMA'
+    ]);
+  });
 
 
